8a7374826e903ff6160a2bd8e737636f17e1d69d
innfi <innfi.world@gmail.com>
2021-01-19 22:43:43 +0900

    testing jwt

---
+++ test/jwt.ts
@@ -0,0 +1,51 @@
+import assert from 'assert';
+import jwt from 'jsonwebtoken';
+
+
+describe('jwt', () => {
+    const jwtObject: object = { secret: 'notsosafesecret' };
+
+    it('current: valid token ', () => {
+        let token = jwt.sign(
+            {email: 'innfi@test.com' },
+            jwtObject['secret'], 
+            { expiresIn: '10m'});
+
+        try {
+            jwt.verify(token, jwtObject['secret']);
+        } catch(err: any) {
+            assert.fail();
+        }
+    });
+
+    it('current: expired token', () => {
+        let token = jwt.sign(
+            {email: 'innfi@test.com', iat: Math.floor(Date.now() / 1000) - 5000 },
+            jwtObject['secret'], 
+            { expiresIn: '2s'});
+            
+        try {
+            const decoded: string | object = jwt.verify(token, jwtObject['secret']);
+        } catch(err: any) {
+            return;
+        }
+
+        assert.fail();
+    });
+
+    it('current: check timestamp', () => {
+        const gap: number = 5000;
+
+        let futureToken = jwt.sign(
+            {email: 'innfi@test.com', iat: Math.floor(Date.now() / 1000) + gap },
+            jwtObject['secret'], { expiresIn: '10m'});
+        let currentToken = jwt.sign(
+            {email: 'innfi@test.com' },
+            jwtObject['secret'], { expiresIn: '10m'});
+
+        const futureDecoded: object = jwt.verify(futureToken, jwtObject['secret']) as object;
+        const currentDecoded: object = jwt.verify(currentToken, jwtObject['secret']) as object;
+
+        assert.strictEqual(futureDecoded['iat'] - currentDecoded['iat'], gap);
+    });
+});
