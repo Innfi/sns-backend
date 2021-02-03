978f28b72c7a0024fb4535e33ebd04cc83d11d66
innfi <innfi.world@gmail.com>
2021-02-03 21:54:57 +0900

    follow: connecting persistence to router

--- src/follows/router.ts
+++ src/follows/router.ts
@@ -1,15 +1,28 @@
 import express from 'express';
 import logger from '../common/logger';
-
+import { followsRepo } from '../persistence/follows/repository';
 
 
 const followsRouter = express.Router();
 
-followsRouter.get('/', async (req: express.Request, res: express.Response) =>  {
+followsRouter.get('/follow', async (req: express.Request, res: express.Response) =>  {
     try {
         logger.info(`follows] get: ${JSON.stringify(req.body)}`);
+
+        //const response = followsRepo.loadFollowsData();
     } catch(err) {
         logger.error(`follows] get error: ${err}`);
+        res.status(500).send('server error').end();
+    }
+});
+
+followsRouter.get('/follower', async (req: express.Request, res: express.Response) => {
+    try {
+        logger.info(`followers] get: ${JSON.stringify(req.body)}`);
+
+        //const response = followsRepo.loadFollowersData();
+    } catch (err) {
+        logger.error(`followers] get error: ${err}`);
         res.status(500).send('server error').end();
     }
 });
--- src/persistence/follows/repository.ts
+++ src/persistence/follows/repository.ts
@@ -1,6 +1,7 @@
 import logger from '../../common/logger';
 import { FollowsAdapter } from './adapter';
-import { IFollows, LoadFollowOptions } from './model';
+import { MockFollowsAdapter } from './mockAdapter';
+import { IFollows, LoadFollowOptions, RelateResult } from './model';
 import { UserProfilePayload } from '../account/model';
 import { accRepo } from '../account/repository';
 
@@ -27,10 +28,44 @@ export class FollowsRepository {
 
             return response;
         } catch(err: any) {
-            logger.error(`loadFollowsData + ${err}`);
+            logger.error(`loadFollowersData] ${err}`);
+            return null;
+        }
+    }
+
+    public async loadFollowsData(userId: string, options: LoadFollowOptions):
+        Promise<UserProfilePayload[] | null> {
+        try {
+            const follows = await this.followsAdapter.loadFollows(userId, options);
+            if(follows === undefined) return null;
+
+            let response: UserProfilePayload[] = [];
+            (follows as Set<string>).forEach(async (value: string) => {
+                const profileResult: UserProfilePayload | null = 
+                    await accRepo.loadUserProfile(value);
+                
+                if(profileResult === null) return;
+
+                response.push(profileResult as UserProfilePayload);
+            });
+
+            return response;
+        } catch (err: any) {
+            logger.error(`loadFollowsData] ${err}`);
+            return null;
+        }
+    }
+
+    //FIXME: relate result
+    public async relate(followId: string, followerId: string): Promise<RelateResult | null> {
+        try {
+            return await this.followsAdapter.relate(followId, followerId);
+        } catch (err: any) {
+            logger.error(`relate] ${err}`);
             return null;
         }
     }
 };
 
 export const followsRepo = new FollowsRepository();
+followsRepo.followsAdapter = new MockFollowsAdapter();
