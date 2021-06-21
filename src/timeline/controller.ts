a6264ea2d8ba6e58eb2c5e17a047840dec1a1487
innfi <innfi.world@gmail.com>
2021-06-21 22:46:56 +0900

    timeline: controller

--- src/timeline/controller.ts
+++ src/timeline/controller.ts
@@ -3,8 +3,8 @@ import { Container, Service } from 'typedi';
 import { useContainer, JsonController, Req, Res, Body, QueryParams, Get, Post } from 'routing-controllers';
 import { Request, Response } from 'express';
 
-import { IUserTimeline } from './model';
-import { TimelineRepository, TimelineRepositoryFactory } from './repository';
+import { IUserTimeline, LoadTimelineOptions, UserTimelineInput } from './model';
+import { TimelineRepository } from './repository';
 import { LoggerBase } from '../common/logger';
 
 
@@ -19,12 +19,42 @@ export class TimelineController {
     ) {}
 
     @Get('/:userId')
-    public async getUserTimeline(@Req() req: Request, @Res() res: Response) {
+    public async getUserTimeline(@Req() req: Request, @Res() res: Response, 
+        @QueryParams() options: LoadTimelineOptions) {
+        try {
+            const userId: string = req.params.userId;
+            this.logger.info(`TimelineController.getUserTimeline] ${userId}`);
+            
+            const timelines: IUserTimeline[] = 
+                await this.tmRepo.loadUserTimeline(userId, options);
 
+            return res.status(200).send({
+                err: 'ok',
+                timelines: timelines
+            }).end();
+        } catch (err: any) {
+            this.logger.error(`TimelineController.getUserTimeline] ${err}`);
+            return res.status(500).end();
+        }
     }
 
     @Post('/:userId')
-    public async writeUserTimeline(@Req() req: Request, @Res() res: Response) {
+    public async writeUserTimeline(@Req() req: Request, @Res() res: Response,
+        @Body() body: UserTimelineInput) { //FIXME: UserTimelineInput
+        try {
+            const userId: string = req.params.userId;
+            this.logger.info(`TimelineController.writeUserTimeline] ${userId}`);
 
+            const result = await this.tmRepo.writeUserTimeline(
+                userId, { authorId: userId, text: body.text});
+            
+            return res.status(200).send({
+                err: 'ok', 
+                newTimeline: result
+            }).end();
+        } catch (err: any) {
+            this.logger.error(`TimelineController.writeUserTimeline] ${err}`);
+            return res.status(500).end();
+        }
     }
 }
